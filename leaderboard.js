e73d1dcacbdf615e2824b27bf28e6f7004f499aa
William <william.taglieber@tha.de>
2025-03-18 22:32:12 +0100

    bufix

--- leaderboard.js
+++ leaderboard.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return response.json();
         }),
-        fetch('player_IDs.json').then(response => {
+        fetch('player_ID.json').then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
             }
