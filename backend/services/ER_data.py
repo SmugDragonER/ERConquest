5ad3098cd295b427d69838b6b5213f70806dd6b8
William <WilliamTaglieber@gmail.com>
2025-12-02 22:13:32 +0100

    Refactoring

--- backend/services/ER_data.py
+++ backend/services/ER_data.py
@@ -1,14 +1,15 @@
+import os, time
 from dotenv import load_dotenv
-import requests
-from ratelimit import limits, sleep_and_retry
-import time, os, json, datetime, pytz
+
+
+### DATA - Start ###
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
 BASE_URL = "https://open-api.bser.io/v1"
-seasonID = 31
-matchingTeamMode = 3
+
+seasonID = 31 #Season 7
+matchingTeamMode = 3 #Squads
 characterCodeDict = {
     1: "Jackie Mini", 2: "Aya Mini", 3: "Fiora Mini", 4: "Magnus Mini", 5: "Zahir Mini",
     6: "Nadine Mini", 7: "Hyunwoo Mini", 8: "Hart Mini", 9: "Isol Mini", 10: "Li Dailin Mini",
@@ -28,21 +29,8 @@ characterCodeDict = {
     76: "Garnet Mini", 77: "Yumin Mini", 78: "Hisui Mini", 79: "Justyna Mini", 80: "István Mini",
     81: "NiaH Mini", 82: "Xuelin Mini"
 }
+### DATA - End ###
 
-session = requests.Session()
-session.headers.update({'x-api-key': API_KEY})
-
-with open("../web/data/player_ID.json", "r", encoding="utf-8") as file:
-    playerIDs = json.load(file)
-
-lockedPlayers=[]
-unlockedPlayers=[]
-currentlyLockedPlayers = 0
-
-@sleep_and_retry
-@limits(calls=2, period=1)
-def rateLimitRequests(url, params = None):
-    return session.get(url, params = params)
 
 def getERData(endpoint: str, params: dict = None, retries: int = 5) -> dict:
     url = f"{BASE_URL}/{endpoint}"
@@ -99,86 +87,3 @@ def getPlayerData(playerName):
         "playerTwitch": twitchLink
     }
     return playerData
-
-def sortPlayers(playerIDs,allPlayerData):
-
-    lockedPlayers = []
-    unlockedPlayers = []
-
-    for player in allPlayerData: 
-        if not playerIDs.get(player["playerName"], {}).get("locked", False):
-            unlockedPlayers.append(player)
-        else:
-            lockedPlayers.append(player)
-
-
-    unlockedPlayers.sort(key=lambda x: x["playerMMR"], reverse=True)
-    lockedPlayers.sort(key=lambda x: playerIDs.get(x["playerName"], {}).get("lockedRank", float('inf')))
-    sortedLeaderboard = unlockedPlayers + lockedPlayers
-
-    return sortedLeaderboard
-
-def saveDatatoJson(data,filename):
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
-
-def saveAllPlayersToJson(playerDataList, filename):
-
-    data = {"players": playerDataList}
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
-
-def saveCurrentTime() -> None :
-    cet_timezone = pytz.timezone("Europe/Paris")
-    currentTime = datetime.datetime.now(cet_timezone)
-
-    if currentTime.minute >= 15 and currentTime.minute < 44:
-        roundedTime = currentTime.replace(minute=30, second=0, microsecond=0)
-
-    elif currentTime.minute >= 45:
-        currentTime = currentTime + datetime.timedelta(hours=1)
-        roundedTime = currentTime.replace(minute=0, second=0, microsecond=0)
-
-    else: #currentTime.minute < 15:
-        roundedTime = currentTime.replace(minute=0, second=0, microsecond=0)
-
-    formattedTime = roundedTime.strftime("Last Update: %d.%m at %H:%M CEST")
-    print(formattedTime)
-    with open("../web/data/last_updated.json", 'w') as json_file:
-        json.dump(formattedTime,json_file, indent=4)
-    print("Current Time Saved")
-
-if __name__ == "__main__":
-    
-    print("data Creation Started")
-    allPlayerData = []
-    for player in playerIDs.keys():
-        try:
-            print(f"Doing {player} atm")
-            playerData = getPlayerData(player)
-            allPlayerData.append(playerData)
-        except Exception as e:
-            print(f"Error with player {player}")
-            # fill everything with 0
-            playerInfo = playerIDs.get(player)
-            twitchLink = playerInfo["twitch"]
-            playerData = {
-        "playerName": player,
-        "playerMMR": 0,
-        "playerGames": 0,
-        "playerWinRate": 0,
-        "playerCharacterStats": None,
-        "playerTwitch": twitchLink
-    }
-            allPlayerData.append(playerData)
-
-    sortedPlayers = sortPlayers(playerIDs,allPlayerData)
-    saveAllPlayersToJson(sortedPlayers, '../web/data/leaderboard_data.json')
-    print("data Creation Done")
-
-    # Used for Player IDs
-    # updatePlayerNum(playerIDs)
-    # saveDatatoJson(playerIDs, "player_ID.json")
-
-    saveCurrentTime()
-    print("Current Time Saved")
